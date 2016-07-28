35f4a1a9844be63d0bfb5ad5f726e221d9b11ef6
Xiupei <xpmei2-c@my.cityu.edu.hk>
2016-07-28 10:45:46 +0800

    - imported view functions from former package - plotD3Graph(g) g is an igraph object

--- R/gsca_summarize.R
+++ R/gsca_summarize.R
@@ -1,7 +1,14 @@
 
+
 ## summarize & default show
 setGeneric("summarize", function(object, what = "ALL", ...)
   standardGeneric("summarize"), package = "HTSanalyzeR2")
+setGeneric("getTopGeneSets", function(object,
+                                      resultName,
+                                      gscs,
+                                      ntop = NULL,
+                                      allSig = FALSE)
+  standardGeneric("getTopGeneSets"), package = "HTSanalyzeR2")
 
 #' @include gsca_class.R
 #' @export
@@ -43,6 +50,60 @@ setMethod("summarize", signature = "GSCA",
               print(object@summary$results, quote = FALSE)
               cat("\n")
             }
+          })
+
+
+##select top significant gene sets
+#' @include gsca_class.R
+#' @export
+setMethod("getTopGeneSets",
+          "GSCA",
+          function(object,
+                   resultName,
+                   gscs,
+                   ntop = NULL,
+                   allSig = FALSE) {
+            ##check arguments
+            if (missing(gscs))
+              stop("Please specify the name(s) of Gene Set Collections in 'gscs'! \n")
+            paraCheck(name = "gscs.names", para = gscs)
+            paraCheck(name = "resultName", para = resultName)
+
+            if (!(resultName %in% names(object@result)))
+              stop("Please input 'HyperGeo.results' or 'GSEA.results'!\n")
+            if (is.null(object@result[[resultName]]))
+              stop("Please run Hypergeometric or GSEA analysis before using this function!\n")
+            gsc.names <- names(object@result[[resultName]])
+            if (!all(gscs %in% gsc.names))
+              stop("Wrong Gene Set Collection name(s) in 'gscs'! \n")
+            if (!is.null(ntop))
+              paraCheck(name = "ntop", para = ntop)
+            paraCheck(name = "allSig", para = allSig)
+
+            if ((is.null(ntop) && !allSig) || (!is.null(ntop) && allSig))
+              stop("Either specify 'ntop' or set 'allSig' to be TRUE!\n")
+            filenames <- list()
+            for (gsc in gscs) {
+              all.gs.names <- rownames(object@result[[resultName]][[gsc]])
+              gs.names <- NULL
+              if (allSig) {
+                gs.names <-
+                  all.gs.names[object@result[[resultName]][[gsc]][, "Adjusted.Pvalue"] < object@para$pValueCutoff]
+              } else {
+                if (ntop > nrow(object@result[[resultName]][[gsc]]))
+                  stop("'ntop' is larger than the number of gene sets in specified gene set collection!\n")
+                gs.names <- all.gs.names[1:ntop]
+              }
+              filenames[[gsc]] <-
+                unlist(lapply(
+                  list(gs.names),
+                  gsub,
+                  pattern = "/",
+                  replacement = "_"
+                ))
+              names(filenames[[gsc]]) <- gs.names
+            }
+            return(filenames)
           })
 
 setMethod("show", signature = "GSCA", function(object) {
