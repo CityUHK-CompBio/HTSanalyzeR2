8a3e9e5a3f829ffdc5086ffa65a65dacd889e706
Xiupei <xpmei2-c@my.cityu.edu.hk>
2016-12-06 22:25:28 +0800

    shiny report refactoring (unfinished)

--- report/app.R
+++ report/app.R
@@ -11,34 +11,11 @@ gscaNodeOpts <- results$gscaNodeOptions
 nwa <- results$nwa
 nwaNodeOpts <- results$nwaNodeOptions
 
+source("common.R")
 
-## write Summary
-generateGSCSummary <- function(gsca) {
-  paste(sapply(1:length(gsca@listOfGeneSetCollections), function(i){
-    (paste("- ", names(gsca@listOfGeneSetCollections)[i],
-           " ( ", length(gsca@listOfGeneSetCollections[[i]]),
-           " gene sets, of which ", gsca@summary$gsc[i, 2],
-           " were above the minimum size )", sep=""))
-  }), collapse = "\n")
-}
-
-generateMethodsSummary <- function(gsca) {
-  summ <- ""
-  if(!is.null(gsca@result$HyperGeo.results)) {
-    summ <- paste(summ, "- Hypergeometric test",
-                  "\n + Significant gene set cutoff p-value (adjusted): ", gsca@summary$para$hypergeo[, "pValueCutoff"],
-                  "\n + MHT correction method: ", gsca@summary$para$hypergeo[, "pAdjustMethod"],
-                  "\n + Minimum gene set size: ", gsca@summary$para$hypergeo[, "minGeneSetSize"], sep = "")
-  }
-  if(!is.null(gsca@result$GSEA.results)) {
-    summ <- paste(summ, "\n\n- Gene Set Enrichment Analysis",
-                  "\n + Significant gene set cutoff p-value (adjusted): ", gsca@summary$para$gsea[, "pValueCutoff"],
-                  "\n + Minimum gene set size: ", gsca@summary$para$gsea[, "minGeneSetSize"],
-                  "\n + MHT correction method: ", gsca@summary$para$gsea[, "pAdjustMethod"],
-                  "\n + Number of permutations: ", gsca@summary$para$gsea[, "nPermutations"],
-                  "\n + Exponent: ", gsca@summary$para$gsea[, "exponent"], sep = "")
-  }
-  summ
+if(!is.null(gsca)) {
+  gsca <- appendLinks(gsca)
+  gsca <- combineResults(gsca)
 }
 
 if(!is.null(gsca)) {
@@ -51,17 +28,6 @@ if(!is.null(nwa)) {
 }
 
 
-createLink <- function(val) {
-  sprintf('<a href="https://www.google.com/#q=%s" target="_blank" class="btn btn-primary">Info</a>',val)
-}
-
-createLink_KEGG <- function(val) {
-  sprintf('<a href="http://www.genome.jp/dbget-bin/www_bget?pathway:%s" target="_blank" class="btn btn-primary">%s</a>',val,val)
-}
-
-createLink_GO <- function(val) {
-  sprintf('<a href="http://www.ebi.ac.uk/QuickGO/GTerm?id=%s" target="_blank" class="btn btn-primary">%s</a>',val,val)
-}
 
 availableResults <- function(results, byRow = TRUE) {
   res <- c("HyperGeo", "GSEA", "Significant in both")
@@ -167,40 +133,6 @@ if(!is.null(gsca) && !is.null(nwa)) {
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-  if(!is.null(gsca)) {
-    ## add gsca terms
-    for(analysis in c("HyperGeo.results", "GSEA.results", "Sig.pvals.in.both", "Sig.adj.pvals.in.both")) {
-      for(genesets in names(gsca@result[[analysis]])) {
-        if(grepl("KEGG", genesets)) {
-          gsca@result[[analysis]][[genesets]] <-
-            data.frame(Gene.Set=createLink_KEGG(rownames(gsca@result[[analysis]][[genesets]])),
-                       gsca@result[[analysis]][[genesets]])
-        } else if(grepl("GO", genesets)) {
-          gsca@result[[analysis]][[genesets]] <-
-            data.frame(Gene.Set=createLink_GO(rownames(gsca@result[[analysis]][[genesets]])),
-                       gsca@result[[analysis]][[genesets]])
-        } else {
-          gsca@result[[analysis]][[genesets]] <-
-            data.frame(Gene.Set=createLink(rownames(gsca@result[[analysis]][[genesets]])),
-                       gsca@result[[analysis]][[genesets]])
-        }
-      }
-    }
-
-    # combine
-    for(name in c("HyperGeo.results", "GSEA.results")) {
-      if(!is.null(gsca@result[[name]])){
-        gsca@result[[name]]$ALL <- do.call(rbind, gsca@result[[name]])
-        gsca@result[[name]]$ALL <- gsca@result[[name]]$ALL[order(gsca@result[[name]]$ALL$Adjusted.Pvalue), ]
-      }
-    }
-
-    if(!is.null(gsca@result$Sig.adj.pvals.in.both)){
-      gsca@result$Sig.adj.pvals.in.both$ALL <- do.call(rbind, gsca@result$Sig.adj.pvals.in.both)
-      gsca@result$Sig.adj.pvals.in.both$ALL <- gsca@result$Sig.adj.pvals.in.both$ALL[order(gsca@result$Sig.adj.pvals.in.both$ALL$HyperGeo.Adj.Pvalue, gsca@result$Sig.adj.pvals.in.both$ALL$GSEA.Adj.Pvalue), ]
-    }
-  }
-
   ## response update
   observeEvent(input$pause, {
     output$network_output <- updateForceGraph(list(pause = input$pause))
@@ -209,13 +141,13 @@ server <- function(input, output, session) {
     output$network_output <- updateForceGraph(list(selection = input$selection))
   })
   observeEvent(input$shape, {
-    output$network_output <- updateForceGraph(list(shape = input$shape, selection = input$selection))
+    output$network_output <- updateForceGraph(list(shape = input$shape))
   })
   observeEvent(input$label_visible, {
-    output$network_output <- updateForceGraph(list(label = input$label_visible, selection = input$selection))
+    output$network_output <- updateForceGraph(list(label = input$label_visible))
   })
   observeEvent(input$scale, {
-    output$network_output <- updateForceGraph(list(scale = input$scale, selection = input$selection))
+    output$network_output <- updateForceGraph(list(scale = input$scale))
   })
   observeEvent(input$charge, {
     output$network_output <- updateForceGraph(list(charge = input$charge))
@@ -233,13 +165,13 @@ server <- function(input, output, session) {
     output$subnetwork_output <- updateForceGraph(list(selection = input$selection2))
   })
   observeEvent(input$shape2, {
-    output$subnetwork_output <- updateForceGraph(list(shape = input$shape2, selection = input$selection2))
+    output$subnetwork_output <- updateForceGraph(list(shape = input$shape2))
   })
   observeEvent(input$label_visible2, {
-    output$subnetwork_output <- updateForceGraph(list(label = input$label_visible2, selection = input$selection2))
+    output$subnetwork_output <- updateForceGraph(list(label = input$label_visible2))
   })
   observeEvent(input$scale2, {
-    output$subnetwork_output <- updateForceGraph(list(scale = input$scale2, selection = input$selection2))
+    output$subnetwork_output <- updateForceGraph(list(scale = input$scale2))
   })
   observeEvent(input$charge2, {
     output$subnetwork_output <- updateForceGraph(list(charge = input$charge2))
@@ -290,55 +222,6 @@ updateNetwork <- function(gsca, input) {
                                  gscs = c(input$genesets2),
                                  allSig=F, ntop = 30, gsNameType=input$nodename,
                                  nodeOptions = gscaNodeOpts, options = options))
-}
-
-selectDT <- function(gsca, input) {
-  if (input$analysis == "Significant in both") {
-    analysis <- "Sig.adj.pvals.in.both"
-    res <- gsca@result[[analysis]][[input$genesets]]
-
-    res$HyperGeo.Adj.Pvalue <- round(res$HyperGeo.Adj.Pvalue, digits = 3)
-    res$GSEA.Adj.Pvalue <- round(res$GSEA.Adj.Pvalue, digits = 3)
-
-    dt <- DT::datatable(res, filter = 'top', rownames = F, escape = F,
-                        options = list(pageLength = 10,
-                                       columnDefs = list(list(
-                                         targets = which(colnames(res) %in% c("HyperGeo.Adj.Pvalue", "GSEA.Adj.Pvalue"))-1,
-                                         render = JS(
-                                           "function(data, type, row, meta) {",
-                                           "return type === 'display' && Number(data) < 0.001 ?",
-                                           "'<0.001' : data;",
-                                           "}")
-                                       ))), callback = JS('table.page(0).draw(false);'))
-
-  } else {
-    analysis <- paste0(input$analysis, ".results")
-    res <- gsca@result[[analysis]][[input$genesets]]
-
-    res$Pvalue <- round(res$Pvalue, digits = 3)
-    res$Adjusted.Pvalue <- round(res$Adjusted.Pvalue, digits = 3)
-
-    dt <- DT::datatable(res, filter = 'top', rownames = F, escape = F,
-                        options = list(pageLength = 10,
-                                       columnDefs = list(list(
-                                         targets = which(colnames(res) %in% c("Pvalue", "Adjusted.Pvalue"))-1,
-                                         render = JS(
-                                           "function(data, type, row, meta) {",
-                                           "return type === 'display' && Number(data) < 0.001 ?",
-                                           "'<0.001' : data;",
-                                           "}")
-                                       ))), callback = JS('table.page(0).draw(false);')) %>%
-
-      formatStyle('Adjusted.Pvalue', target = "row",
-                  fontWeight = styleInterval(gsca@para$pValueCutoff, c('bold', 'weight'))
-                  # backgroundColor = styleInterval(gsca@para$pValueCutoff, c('green', 'gray')),
-      )
-
-    if (analysis == "HyperGeo.results") dt <- dt %>% formatRound("Expected.Hits", digits = 3)
-    if (analysis == "GSEA.results") dt <- dt %>% formatSignif("Observed.score", digits = 3)
-
-  }
-  dt
 }
 
 
