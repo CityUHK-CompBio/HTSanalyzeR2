8a8fc70ed4f31e49ead974028ff963d057dc385f
Xiupei <xpmei2-c@my.cityu.edu.hk>
2017-10-30 16:52:26 +0000

    Hide setting button on table page.

--- inst/htmlwidgets/lib/btn.js
+++ inst/htmlwidgets/lib/btn.js
@@ -5,6 +5,8 @@ $(function() {
 		$('#settingBar').toggleClass('active');
 	});
 
+	var firstTab = $('section.sidebar ul.sidebar-menu li a').first().attr("href");
+	$('li.messages-menu').css("display", firstTab == "#shiny-tab-table_tab" ? "none" : "block");
 	
 	$(".tab-content .tab-pane").first().addClass("active");
 
@@ -13,7 +15,10 @@ $(function() {
 		$(".tab-content .tab-pane.active").removeClass("active")
 		var tabId = $("a", ev.currentTarget).attr("href");
 		$(tabId).addClass("active");
-		
+
+		$('li.messages-menu').css("display", tabId == "#shiny-tab-table_tab" ? "none" : "block");
+		$('#settingBar').removeClass('active');
+
 		tabSwitched(tabId);
 	});
 
