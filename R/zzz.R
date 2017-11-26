f5205cf6293f5055b5863ef8e840e67aebdae18f
linazhu3 <zhulina_hmu@163.com>
2017-11-26 13:43:12 +0000

    change dependent R version of devel branch back to 3.4

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 }
 
 .onLoad <- function(libname, pkgname) {
-  if(getRversion() >= "3.5") {
+  if(getRversion() >= "3.4") {
     utils::globalVariables(names = c("idx", "tags"), add=FALSE, package = "HTSanalyzeR2")
     }
 }
