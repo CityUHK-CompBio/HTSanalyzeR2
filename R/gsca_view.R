421fdec0eddde88d0886a05114b97bbf189c2037
linazhu3 <zhulina_hmu@163.com>
2019-03-02 03:16:27 +0800

    GSEA plot revision

--- R/gsca_view.R
+++ R/gsca_view.R
@@ -1,13 +1,17 @@
 ## summarize & default show
 if(!isGeneric("viewGSEA"))
-  setGeneric("viewGSEA",function(object,...) standardGeneric("viewGSEA"), package="HTSanalyzeR2")
+  setGeneric("viewGSEA",function(object,...)
+    standardGeneric("viewGSEA"), package="HTSanalyzeR2")
 if(!isGeneric("plotGSEA"))
-  setGeneric("plotGSEA",function(object,...) standardGeneric("plotGSEA"), package="HTSanalyzeR2")
+  setGeneric("plotGSEA",function(object,...)
+    standardGeneric("plotGSEA"), package="HTSanalyzeR2")
+
+
 #' Plot a figure of GSEA results for one gene set
 #'
-#' This is a generic function. When implemented as the S4 method for objects of class GSCA,
-#'  this function plots a figure
-#' of the positions of the gene sets in the ranked gene list and the location of the enrichment score.
+#' This is a generic function. When implemented as the S4 method for
+#' objects of class GSCA, this function plots a figure of the positions
+#' of the gene sets in the ranked gene list and the location of the enrichment score.
 #'
 #' @details
 #' We suggest to print the names of top significant gene sets using the
@@ -16,6 +20,17 @@ if(!isGeneric("plotGSEA"))
 #' @param gscName A single character value specifying the name of the gene set collection where
 #' the gene set is.
 #' @param gsName A single character value specifying the name of the gene set to be plotted.
+#' @param ES.range A numeric vector for user-defined range of enrichment score showing in the
+#' top part of GSEA plot. Default is the range of actual enrichment score of result. However, users are
+#' also allowed to set by themselves for better comparison with other GSEA plots.
+#' @param rankMetric.range A numeric vector for user-defined range of ranked gene lists showing in the
+#' bottom part of GSEA plot. Default is the range of actual ranked gene lists of result. However, users are
+#' also allowed to set by themselves for better comparison with other GSEA plots.
+#' @param main.title A single character value specifying the main title of the GSEA plot. Default
+#' is the name of the gene set.
+#' @param ESline.col The color to be used for enrichment score profile line. Defaults to "FireBrick".
+#' @param hits.col The color to be used for hits line to show the position of hits. Defaults to "CadetBlue".
+#' @param rankMetric.col The color to be used for ranked metric line. Defaults to "CadetBlue".
 #' @rdname viewGSEA
 #' @aliases viewGSEA
 #' @return In the end, this function would draw a GSEA figure for specified gene set.
@@ -32,13 +47,26 @@ if(!isGeneric("plotGSEA"))
 #' ## view GSEA results for one gene set
 #' \dontrun{
 #' viewGSEA(d7_gsca, "GO_MF", topGS_GO_MF[["GO_MF"]][1])
+#'
+#' ## view GSEA results for the same gene set by user defined enrichment score range,
+#' ## ranked gene list range and enrichment score profile line color.
+#' viewGSEA(d7_gsca, "GO_MF", topGS_GO_MF[["GO_MF"]][1],
+#' ES.range = c(-1, 0), rankMetric.range = c(-5, 2),
+#' ESline.col = "green")
 #' }
+#'
 #' @include gsca_class.R
 #' @export
 setMethod(
   "viewGSEA",
   "GSCA",
-  function(object, gscName, gsName) {
+  function(object, gscName, gsName,
+           ES.range = NULL,
+           rankMetric.range = NULL,
+           main.title = NULL,
+           ESline.col = "FireBrick",
+           hits.col = "black",
+           rankMetric.col = "CadetBlue") {
     ##check argument
     paraCheck("Report", "gs.single", gsName)
     paraCheck("Report", "gsc.name", gscName)
@@ -52,17 +80,27 @@ setMethod(
     if(!(gscName %in% names(object@listOfGeneSetCollections)))
       stop("'gsc' is not a gene set collection in 'listOfGeneSetCollections'!\n")
 
-    test <- gseaScores(geneList = object@geneList, geneSet = object@listOfGeneSetCollections[[gscName]][[gsName]],
-                       exponent = object@para$exponent, mode = "graph", GSEA.results = object@result$GSEA.results[[gscName]],
+    test <- gseaScores(geneList = object@geneList,
+                       geneSet = object@listOfGeneSetCollections[[gscName]][[gsName]],
+                       exponent = object@para$exponent,
+                       mode = "graph",
+                       GSEA.results = object@result$GSEA.results[[gscName]],
                        gsName = gsName)
     gseaPlots(runningScore = test[['runningScore']],
               enrichmentScore = test[['enrichmentScore']],
               positions = test[['positions']], geneList = object@geneList,
               Adjust.P.value = test[['Adjust.P.value']],
               gsName = gsName,
-              nPermutations = object@summary$para$gsea[, "nPermutations"])
+              nPermutations = object@summary$para$gsea[, "nPermutations"],
+              ES.range = ES.range,
+              rankMetric.range = rankMetric.range,
+              main.title = main.title,
+              ESline.col = ESline.col,
+              hits.col = hits.col,
+              rankMetric.col = rankMetric.col)
   }
 )
+
 
 #' Plot and save figures of GSEA results for top significant gene sets
 #'
@@ -80,6 +118,15 @@ setMethod(
 #'   sets will be plotted.
 #' @param filepath A single character value specifying where to store GSEA figures.
 #' @param output A single character value specifying the format of output image: "pdf" or "png".
+#' @param ES.range A numeric vector for user-defined range of enrichment score showing in the
+#' top part of GSEA plot. Default is the range of actual enrichment score of result. However, users are
+#' also allowed to set by themselves for better comparison with other GSEA plots.
+#' @param rankMetric.range A numeric vector for user-defined range of ranked gene lists showing in the
+#' bottom part of GSEA plot. Default is the range of actual ranked gene lists of result. However, users are
+#' also allowed to set by themselves for better comparison with other GSEA plots.
+#' @param ESline.col The color to be used for enrichment score profile line. Defaults to "FireBrick".
+#' @param hits.col The color to be used for hits line to show the position of hits. Defaults to "CadetBlue".
+#' @param rankMetric.col The color to be used for ranked metric line. Defaults to "CadetBlue".
 #' @param ... Other arguments used by the function png or pdf such as 'width' and 'height'
 #' @rdname plotGSEA
 #' @return In the end, this function would plot GSEA figure and store them into the specified path.
@@ -100,7 +147,12 @@ setMethod(
 setMethod(
   "plotGSEA",
   "GSCA",
-  function(object, gscs, ntop=NULL, allSig=FALSE, filepath=".", output="pdf", ...) {
+  function(object, gscs, ntop=NULL, allSig=FALSE, filepath=".", output="pdf",
+           ES.range = NULL,
+           rankMetric.range = NULL,
+           ESline.col = "FireBrick",
+           hits.col = "black",
+           rankMetric.col = "CadetBlue", ...) {
     ##check arguments
     paraCheck("Report", "filepath", filepath)
     paraCheck("Report", "output", output)
@@ -120,6 +172,11 @@ setMethod(
                         GSEA.results = object@result$GSEA.results[[gsc]],
                         gsName = gs.name,
                         nPermutations = object@summary$para$gsea[, "nPermutations"],
+                        ES.range = ES.range,
+                        rankMetric.range = rankMetric.range,
+                        ESline.col = ESline.col,
+                        hits.col = hits.col,
+                        rankMetric.col = rankMetric.col,
                         ...=...)
         }
       }
@@ -128,11 +185,22 @@ setMethod(
 )
 
 
+
 #' @import graphics
 #' @importFrom grDevices colorRampPalette
-gseaPlots <- function(runningScore, enrichmentScore,
-                      positions, geneList, Adjust.P.value,
-                      gsName, nPermutations) {
+gseaPlots <- function(runningScore,
+                      enrichmentScore,
+                      positions,
+                      geneList,
+                      Adjust.P.value,
+                      gsName,
+                      nPermutations,
+                      ES.range = NULL,
+                      rankMetric.range = NULL,
+                      main.title = NULL,
+                      ESline.col = "FireBrick",
+                      hits.col = "black",
+                      rankMetric.col = "CadetBlue") {
 
   ## check arguments
   paraCheck("GSCAClass", "genelist", geneList)
@@ -152,28 +220,39 @@ gseaPlots <- function(runningScore, enrichmentScore,
   if(length(positions) != length(geneList))
     stop("The length of 'positions' should be the same as the length of 'geneList'!\n")
 
+  ## check the user defined range and mian title, etc.
+  if(is.null(ES.range)){
+    ES.range <- c(min(runningScore), max(runningScore))
+  }
+
+  if(is.null(rankMetric.range)){
+    rankMetric.range <- c(min(geneList), max(geneList))
+  }
+
+  if(is.null(main.title)){
+    main.title <- gsName
+  }
+
   ##set the graphical parameters
   def.par <- par(no.readonly = TRUE)
-  gsea.layout <- layout(matrix(c(1, 2, 3)), heights = c(0.9,0.3,0.1))
-  par(mai=c(0, 1.2, 0.5, 0.2), cex.axis=2)
+  # Create a division of the device
+  gsea.layout <- layout(matrix(c(1, 2, 3, 4)), heights = c(2, 0.5, 0.2, 1.7))
 
   ##Plot the running score and add a vertical line at the position of
   ##the enrichment score (maximal absolute value of the running score)
+  par(mar = c(0, 5, 2, 2), cex.axis=1.4, cex.lab = 1.6, mgp = c(3.3,1,0))
   plot(x=c(1:length(runningScore)), y=runningScore, type="l",
-      lwd=3, col="green",
-      # cex.lab = 2.5,
-      ylab="", yaxt = "n",
-      bg = "aliceblue", xaxt = "n")
-  label <- seq(round(min(runningScore), digits = 1),
-               round(max(runningScore), digits = 1),
+      lwd=3, col=ESline.col,
+      xaxt = "n", xaxs = "i",
+      yaxt = "n", ylab="Enrichment score (ES)",
+      ylim = ES.range)
+  label <- seq(round(min(ES.range), digits = 1),
+               round(max(ES.range), digits = 1),
                by = 0.1)
   axis(2, at=label,
        labels=label, las=1)
-  title(main = gsName, cex.main = 2.5,
-        ylab="Enrichment score (ES)",
-        mgp=c(4.5, 1, 0),
-        cex.lab=2)
-  grid(NULL, NULL, lwd = 2)
+  title(main = main.title, cex.main = 1.8)
+  grid(NULL, NULL, lwd = 2.5)
   abline(h=0, lty = 2)
 
   nPermutations <- as.numeric(nPermutations)
@@ -184,26 +263,26 @@ gseaPlots <- function(runningScore, enrichmentScore,
                       ifelse(Adjust.P.value == 0,
                              paste0("< ", minP),
                              paste0("= ", signif(Adjust.P.value, 2)))),
-       cex = 2)} else {
+       cex = 1.5)} else {
          text(x = length(geneList)/6, y = (enrichmentScore)/4*3,
               labels = paste("ES = ", signif(enrichmentScore, 3), "\nFDR ",
                              ifelse(Adjust.P.value == 0,
                                     paste0("< ", minP),
                                     paste0("= ", signif(Adjust.P.value, 2)))),
-              cex = 2)
+              cex = 1.5)
        }
   #-------------------## plot a color barplot indicating the phenotypes
-  par(mai=c(0, 1.2, 0, 0.2))
+  par(mar = c(0, 5, 0, 2))
   plot(0, type = "n", xaxt = "n", xaxs = "i", xlab = "", yaxt = "n",
        ylab = "", xlim = c(1, length(geneList)), lwd = 3)
-  abline(v=which(positions == 1), lwd = 0.75)
+  abline(v=which(positions == 1), lwd = 0.75, col = hits.col)
   #------------------------------------------------------------------
   ##Plot the phenotypes along the geneList, and add a vertical line
   ##for each match between geneList and gene set
   ##this is done using the 'positions' output of gseaScores,
   ##which stores a one for each match position and a zero otherwise
 
-  par(mai = c(0.1, 1.2, 0, 0.2))
+  par(mar = c(0, 5, 0, 2))
   ticks = 1000
   ran <- range(geneList, na.rm = TRUE)
   offset <- ceiling(ticks * ran[1] / (ran[1] - ran[2]))
@@ -217,8 +296,31 @@ gseaPlots <- function(runningScore, enrichmentScore,
   barplot(matrix(rank.colors$lengths), col = rank.colors$values,
           border = NA, horiz = TRUE,xaxt = "n", xlim = c(1, length(geneList)))
   box()
-  text(length(geneList) * 0.01, 0.7, "Positive", adj = c(0, NA), cex = 1.7)
-  text(length(geneList) * 0.99, 0.7, "Negative", adj = c(1, NA), cex = 1.7)
+  text(length(geneList) * 0.01, 0.7, "Positive", adj = c(0, NA), cex = 1.4)
+  text(length(geneList) * 0.99, 0.7, "Negative", adj = c(1, NA), cex = 1.4)
+
+  ## plot rank metric bar
+  par(mar = c(5, 5, 0, 2), cex.axis=1.4, cex.lab = 1.6, mgp = c(3,1,0))
+  rank.metric <- rle(round(geneList, digits = 2))
+  plot(geneList, type = "n", xaxs = "i",
+       xlab = "Rank in ordered gene list",
+       xlim = c(0, length(geneList)),
+       ylim = rankMetric.range, yaxs = "i",
+       ylab = "Ranked list metric", yaxt = "n")
+  label <- seq(round(min(geneList), digits = 0),
+               round(max(geneList), digits = 0),
+               by = 1)
+  axis(2, at=label,
+       labels=label,las=1)
+  grid(NULL, NULL, lwd = 2.5)
+  barplot(rank.metric$values, col = rankMetric.col, lwd = 1,
+          xaxs = "i", xaxt = "n", yaxt = "n",
+          xlim = c(0, length(geneList)),
+          ylim = c(-1, 1), yaxs = "i",
+          width = rank.metric$lengths, border = NA,
+          space = 0, add = TRUE)
+  box()
+
   par(def.par)  #- reset to default
 }
 
@@ -229,7 +331,12 @@ makeGSEAplots <- function(geneList, geneSet, exponent, filepath,
                           filename, output,
                           GSEA.results,
                           gsName,
-                          nPermutations = object@summary$para$gsea[, "nPermutations"],
+                          nPermutations,
+                          ES.range = NULL,
+                          rankMetric.range = NULL,
+                          ESline.col,
+                          hits.col,
+                          rankMetric.col,
                           ...) {
   test <- gseaScores(geneList = geneList, geneSet = geneSet,
                      exponent = exponent, mode = "graph", GSEA.results = GSEA.results,
@@ -247,7 +354,12 @@ makeGSEAplots <- function(geneList, geneSet, exponent, filepath,
             positions = test[['positions']], geneList = geneList,
             Adjust.P.value = test[['Adjust.P.value']],
             gsName = gsName,
-            nPermutations = object@summary$para$gsea[, "nPermutations"])
+            nPermutations = nPermutations,
+            ES.range = ES.range,
+            rankMetric.range = rankMetric.range,
+            ESline.col = ESline.col,
+            hits.col = hits.col,
+            rankMetric.col = rankMetric.col)
   dev.off()
 }
 
@@ -269,6 +381,8 @@ gseaScores <- function(geneList, geneSet, exponent=1, mode="score", gsName = gsN
   Pmiss <- rep(0, N)
   runningES <- rep(0, N)
 
+
+  ## calculate the enrichment score
   if(nh > N) {
     stop("Gene Set is larger than Gene List")
   } else {
@@ -287,7 +401,8 @@ gseaScores <- function(geneList, geneSet, exponent=1, mode="score", gsName = gsN
     }
   }
   ## get GSEA adjust pvalues
-  adjust.p.value <- GSEA.results[which(rownames(GSEA.results) == gsName), "Adjusted.Pvalue"]
+  adjust.p.value <- GSEA.results[which(rownames(GSEA.results) == gsName),
+                                 "Adjusted.Pvalue"]
   ## Return the relevant information according to mode
   if(mode=="score")
     return(ES)
